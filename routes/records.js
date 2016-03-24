a99421780f86d699847da40c24a1fea07433cf0b
Slevin Zhang <4ce111@gmail.com>
2016-03-24 16:36:21 -0400

    Improve records.js format according to style

--- routes/records.js
+++ routes/records.js
@@ -3,67 +3,68 @@ var express = require('express');
 var api = express.Router();
 
 api.route('/records')
-.get(function(req, res) {
-  models.Record.findAll({
-    include: [
-      { model: models.User, attributes: ['name', 'email', 'privilege'] },
-      models.Location,
-      models.Type
-    ]
-  }).then(function(records) {
-    res.json({ records: records });
-  }, function(error) {
-    res.status(500).json({ message: 'Failed to find records', error: error });
-  });
-})
-.post(function(req, res) {
-  var defaultLocation = {
-    longitude: req.body.long,
-    latitude: req.body.lat
-  };
-  if (req.body.address) defaultLocation.address = req.body.address;
-  models.Location.findOrCreate({
-    where: {
+  .get(function getRecord(req, res) {
+    models.Record.findAll({
+      include: [
+        { model: models.User, attributes: ['name', 'email', 'privilege'] },
+        models.Location,
+        models.Type
+      ]
+    }).then(function ffFindAllRecords(records) {
+      res.json({ records: records });
+    }).catch(function getRecordCatchAll(error) {
+      res.status(500).json({ message: 'Error finding records', error: error });
+    });
+  })
+  .post(function postRecord(req, res) {
+    var defaultLocation = {
       longitude: req.body.long,
       latitude: req.body.lat
-    },
-    defaults: defaultLocation
-  }).spread(function(location, created) {
-    return models.Record.create({
-      description: req.body.description,
-      severity: req.body.severity,
-      UserId: 1,
-      LocationId: location.id,
-      TypeId: req.body.type
+    };
+    if (req.body.address) defaultLocation.address = req.body.address;
+    models.Location.findOrCreate({
+      where: {
+        longitude: req.body.long,
+        latitude: req.body.lat
+      },
+      defaults: defaultLocation
+    }).spread(function ffFindOrCreateLocation(location, created) {
+      return models.Record.create({
+        description: req.body.description,
+        severity: req.body.severity,
+        UserId: req.body.user_id,
+        LocationId: location.id,
+        TypeId: req.body.type
+      });
+    }).then(function ffCreateRecord(record) {
+      res.status(201).json({ message: 'Created record', record: record });
+    }).catch(function postRecordCatchAll(error) {
+      switch (error.name) {
+        case 'SequelizeValidationError':
+          res.status(400).json({ message: 'Invalid fields', details: error });
+          break;
+        default:
+          res.status(500).json({
+            message: 'Error creating record', details: error
+          });
+      }
     });
-  }, function(error) {
-    var failMessage = 'Failed to find or create location';
-    if (error.name == 'SequelizeValidationError')
-      return res.status(400).json({ message: failMessage, error: error });
-    res.status(500).json({ message: failMessage, error: error });
-  }).then(function(record) {
-    res.status(201).json({ message: 'Created record', record: record });
-  }, function(error) {
-    var failMessage = 'Failed to create record';
-    switch (error.name) {
-      case 'SequelizeValidationError':
-        res.status(400).json({ message: failMessage, error: error });
-        break;
-      default:
-        res.status(500).json({ message: failMessage, error: error });
-    }
   });
-});
 
 api.route('/records/:user_id')
-.get(function(req, res) {
-  models.Record.count({
-    where: { UserId: req.params.user_id }
-  }).then(function(count) {
-    res.json({ count: count });
-  }, function(error) {
-    res.status(500).json(error);
+  .get(function getRecordByUserId(req, res) {
+    models.Record.findAll({
+      where: {
+        UserId: req.params.user_id
+      }
+    }).then(function ffFindRecordsByUserId(records) {
+      res.json({ records: records });
+    }).catch(function findRecordsByUserIdCatchAll(error) {
+      res.status(500).json({
+        message: 'Error finding records by user id ' + req.params.user_id,
+        details: error
+      });
+    });
   });
-});
 
 module.exports = api;
