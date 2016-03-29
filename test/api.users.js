1ff8bcc58a3f12a5380ef8288ee9e073e50f547b
Slevin Zhang <4ce111@gmail.com>
2016-03-29 08:41:58 -0400

    Implement tests for PUT /user/:user_id

--- test/api.users.js
+++ test/api.users.js
@@ -7,6 +7,7 @@ var server = supertest.agent('http://localhost:3000');
 describe('/api/user*', function() {
   this.slow(250);
   var token;
+  var standardToken; // non admin
 
   before(function(done) {
     models.User.create({
@@ -49,7 +50,6 @@ describe('/api/user*', function() {
 
 
   describe('POST /users', function() {
-    var standardToken; // non admin
     before(function(done) {
       server
         .post('/api/authenticate')
@@ -160,4 +160,116 @@ describe('/api/user*', function() {
     });
   });
 
+
+  describe('PUT /user/:user_id', function() {
+
+    it('should return 400 status if no input provided', function(done) {
+      server
+        .put('/api/user/1')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .end(function(err, res) {
+          res.status.should.equal(400);
+          res.body.message.should.equal('No input provided');
+          done();
+        });
+    });
+
+    it('should return 401 status if non admin try to update privilege', function(done) {
+      server
+        .put('/api/user/2')
+        .send({ privilege: 'A' })
+        .set('Authorization', standardToken)
+        .expect('Content-Type', /json/)
+        .expect(401)
+        .end(function(err, res) {
+          res.status.should.equal(401);
+          res.body.message.should.equal('User is not an admin');
+          done();
+        });
+    });
+
+    it('should return 401 status if non admin try to reset password', function(done) {
+      server
+        .put('/api/user/2')
+        .send({ password: 'null', active: 'No' })
+        .set('Authorization', standardToken)
+        .expect('Content-Type', /json/)
+        .expect(401)
+        .end(function(err, res) {
+          res.status.should.equal(401);
+          res.body.message.should.equal('User is not an admin');
+          done();
+        });
+    });
+
+    it('should return 401 status if a user try to update another\'s name', function(done) {
+      server
+        .put('/api/user/2')
+        .send({ name: 'awesome' })
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(401)
+        .end(function(err, res) {
+          res.status.should.equal(401);
+          res.body.message.should.equal('Can only self-rename');
+          done();
+        });
+    });
+
+    it('should retrun 400 status if a user does not exist', function(done) {
+      server
+        .put('/api/user/100')
+        .send({ privilege: 'A' })
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .end(function(err, res) {
+          res.status.should.equal(400);
+          res.body.message.should.equal('User does not exist');
+          done();
+        });
+    });
+
+    // TODO only self set password (without using user/1 token)
+    it('should be able to set a user\'s password', function(done) {
+      server
+        .put('/api/user/4')
+        .send({ password: 'newpassword' })
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          var affected = res.body.affected;
+          var affectedUser = affected[1][0];
+          res.status.should.equal(200);
+          res.body.message.should.equal('Updated user');
+          affected[0].should.equal(1);
+          affectedUser.password.length.should.equal(60);
+          affectedUser.active.should.equal(true);
+          done();
+        });
+    });
+
+    it('should be able to reset a user\'s password', function(done) {
+      server
+        .put('/api/user/4')
+        .send({ password: 'null', active: 'No' })
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+          var affected = res.body.affected;
+          var affectedUser = affected[1][0];
+          res.status.should.equal(200);
+          res.body.message.should.equal('Updated user');
+          affected[0].should.equal(1);
+          should(affectedUser.password).be.exactly(null);
+          affectedUser.active.should.equal(false);
+          done();
+        });
+    });
+
+  });
 });
